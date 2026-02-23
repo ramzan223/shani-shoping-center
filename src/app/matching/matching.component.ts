439a05baccc0e4f9fea29ef73fc9f2fd7b8e5529
Ramzan Khan <raqzankhan@gmail.com>
2026-02-23 12:44:36 +0500

    ready for deploy

--- src/app/matching/matching.component.ts
+++ src/app/matching/matching.component.ts
@@ -7,5 +7,14 @@ import { Component } from '@angular/core';
   styleUrl: './matching.component.scss'
 })
 export class MatchingComponent {
+scrollToSection(sectionId: string) {
+  const element = document.getElementById(sectionId);
 
+  if (element) {
+    element.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+  }
+}
 }
